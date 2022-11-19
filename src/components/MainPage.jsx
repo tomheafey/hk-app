7809165783e06a518a2b0f96c61cc7fa34843a9c
Tom Heafey <tmheafey@gmail.com>
2022-11-18 20:09:05 -0600

    sketches out potential options in optionsmenu

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,10 +2,12 @@ import CharmSelector from "../shared/components/CharmSelector";
 
 import React from "react";
 import CurrentCharms from "../shared/components/CurrentCharms";
+import OptionsMenu from "../shared/components/OptionsMenu";
 
 const MainPage = () => {
     return (
         <>
+            <OptionsMenu />
             <CurrentCharms />
             <CharmSelector />;
         </>
--- src/shared/components/OptionsMenu.jsx
+++ src/shared/components/OptionsMenu.jsx
@@ -1,4 +1,6 @@
 //probably a better name for this
+//! may want to move this onto an 'intro page' to not clutter the main page
+//! or at least make it hideable/collapsible
 
 //select nail level
 //radios? for
@@ -9,3 +11,66 @@
 //  desolate dive vs descending dark
 //  howling wraiths vs abyss shriek
 //should DEFAULT everything to 112% completion status
+
+import React, { useState } from "react";
+
+const OptionsMenu = () => {
+    const [nailLevel, setNailLevel] = useState("pure");
+    const nailLevels = [
+        { val: "pure", label: "Pure Nail" },
+        { val: "coiled", label: "Coiled Nail" },
+        { val: "channelled", label: "Channelled Nail" },
+        { val: "sharpened", label: "Sharpened Nail" },
+        { val: "old", label: "Old Nail" },
+    ];
+
+    return (
+        <>
+            <label htmlFor="nail-level">Nail level</label>
+            <select id="nail-level" value={nailLevel} onChange={(e) => setNailLevel(e.target.value)}>
+                {nailLevels.map((n) => (
+                    <option key={n.val} value={n.val}>
+                        {n.label}
+                    </option>
+                ))}
+            </select>
+
+            <div>
+                <input type="radio" name="vh-ks" id="void-heart" defaultChecked />
+                <label htmlFor="void-heart">Void Heart</label>
+                <input type="radio" name="vh-ks" id="kingsoul" />
+                <label htmlFor="kingsoul">Kingsoul</label>
+            </div>
+
+            <div>
+                <input type="radio" name="cm-gc" id="carefree-melody" defaultChecked />
+                <label htmlFor="carefree-melody">Carefree Melody</label>
+                <input type="radio" name="cm-gc" id="grimmchild" />
+                <label htmlFor="grimmchild">Grimmchild</label>
+            </div>
+
+            <div>
+                <input type="radio" name="ss-vs" id="shade-soul" defaultChecked />
+                <label htmlFor="shade-soul">Shade Soul</label>
+                <input type="radio" name="ss-vs" id="vengeful-spirit" />
+                <label htmlFor="vengeful-spirit">Vengeful Spirit</label>
+            </div>
+
+            <div>
+                <input type="radio" name="dd-dd" id="descending-dark" defaultChecked />
+                <label htmlFor="descending-dark">Descending Dark</label>
+                <input type="radio" name="dd-dd" id="desolate-dive" />
+                <label htmlFor="desolate-dive">Desolate Dive</label>
+            </div>
+
+            <div>
+                <input type="radio" name="as-hw" id="abyss-shriek" defaultChecked />
+                <label htmlFor="abyss-shriek">Abyss Shriek</label>
+                <input type="radio" name="as-hw" id="howling-wraiths" />
+                <label htmlFor="howling-wraiths">Howling Wraiths</label>
+            </div>
+        </>
+    );
+};
+
+export default OptionsMenu;
